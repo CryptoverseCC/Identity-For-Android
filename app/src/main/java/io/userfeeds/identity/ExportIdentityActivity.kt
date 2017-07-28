d4204c67f1fb3cab96fc7214c2b7ab659adcf998
Maciej Górski <maciek.gorski@gmail.com>
2017-07-28 10:54:05 +0200

    Block taking screenshots when private key is displayed.

--- app/src/main/java/io/userfeeds/identity/ExportIdentityActivity.kt
+++ app/src/main/java/io/userfeeds/identity/ExportIdentityActivity.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager.LayoutParams.FLAG_SECURE
 import kotlinx.android.synthetic.main.export_identity_activity.*
 import net.glxn.qrgen.android.QRCode
 
@@ -14,6 +15,7 @@ class ExportIdentityActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(FLAG_SECURE, FLAG_SECURE)
         setContentView(R.layout.export_identity_activity)
         copyToClipboardView.setOnClickListener { copyToClipboard() }
         shareView.setOnClickListener { share() }
